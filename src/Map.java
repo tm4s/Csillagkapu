cde9c142c2b0e73ef21a24ce3e8dc20040865c30
tm4s <tmas.szanto@gmail.com>
2016-03-01 23:34:11 +0100

    Map.java
    inicializalas vaza 1.0

--- src/Map.java
+++ src/Map.java
@@ -3,11 +3,17 @@ public class Map {
     private int width;
     private int height;
 
-    private Field[][] data = new Field[height][width];
+    private Field[][] data;
 
-    public Map (Size size, String fileName) {
+    public Map(String fileName) {
+
+        Size size = getMapSize(fileName);
         width = size.getX();
         height = size.getY();
+
+        data = new Field[height][width];
+
+        readMapData(fileName);
     }
 
     public int getWidth() {
@@ -16,5 +22,21 @@ public class Map {
 
     public int getHeight() {
         return height;
+    }
+
+    private Size getMapSize(String fileName) {
+
+        // kikene olvasni a palya meretet a fajlbol
+
+        Size size = new Size(20, 10);     // csakohogy legyen valami
+
+        return size;
+
+    }
+
+    private void readMapData(String fileName) {
+
+        // data tabla feltoltese adatokkal
+
     }
 }
