6434b9c80e90d6fe0e610a32b9a579a75be64c06
tm4s <tmas.szanto@gmail.com>
2016-03-02 03:19:56 +0100

    Coordinate.java add, equals, constructor

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -11,6 +11,19 @@ public class Coordinate {
         this.y = y;
     }
 
+    public Coordinate(Coordinate coordinate) {
+        x = coordinate.getX();
+        y = coordinate.getY();
+    }
+
+    public Coordinate add(Coordinate coordinate) {
+        return new Coordinate(this.x + coordinate.getX(), this.y + coordinate.getY());
+    }
+
+    public  boolean equals(Coordinate coordinate) {
+        return (x == coordinate.getX()) && (y == coordinate.getY());
+    }
+
     public int getX() {
         return x;
     }
