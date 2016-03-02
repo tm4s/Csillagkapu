c4e12a589590c768eed9d88df599fe6c34233846
tm4s <tmas.szanto@gmail.com>
2016-03-02 16:31:19 +0100

    Door vaz

--- src/Door.java
+++ src/Door.java
@@ -1,5 +1,28 @@
 /**
  * Ajtó
  */
+
 public class Door extends Field {
+
+
+    public Door(Coordinate position) {
+        super(position);
+    }
+
+    @Override
+    public void collideWith(Colonel colonel) {
+
+    }
+
+    @Override
+    public void collideWith(Bullet bullet) {
+
+    }
+
+    public void open() {
+    }
+
+    public void close() {
+    }
+
 }
