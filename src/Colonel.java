29b39d3997869783d8b523766a074869deff382c
tm4s <tmas.szanto@gmail.com>
2016-03-02 03:21:04 +0100

    Colonel.java collideWithEmptyField
    kezdemeny meg csak

--- src/Colonel.java
+++ src/Colonel.java
@@ -2,6 +2,26 @@
  * Ezredes
  */
 
-public class Colonel extends Field{
+public class Colonel{
+    private Coordinate position;
 
+    public Colonel(Coordinate position) {
+        position = new Coordinate(position);
+    }
+
+    public void collideWith(Field field) {
+        field.accept(this);
+    }
+
+    public void collideWith(EmptyField emptyField) {
+        setPosition(emptyField.getPosition());
+    }
+
+    public void setPosition(Coordinate position) {
+        this.position = new Coordinate(position);
+    }
+
+    public Coordinate getPosition() {
+        return position;
+    }
 }
