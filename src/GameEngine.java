2ff4ba6c70fdee08615527fffb9403b4c7d9d9b9
tm4s <tmas.szanto@gmail.com>
2016-03-02 03:21:39 +0100

    GameEngine.java
    ezredes ures mezore lep kezdemeny

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -3,4 +3,17 @@
  */
 
 public class GameEngine {
+    private Map map;
+    private Colonel colonel;
+
+    public  GameEngine() {
+        map = new Map("mapName");
+        colonel = map.getColonelStart();
+    }
+
+    public void moveColonelTo(Coordinate vecDst) {
+        Coordinate posDst = vecDst.add(colonel.getPosition());
+        colonel.collideWith(map.getFieldAt(posDst));
+    }
+
 }
