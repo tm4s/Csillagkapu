3dce72c68528c31c346588798b5989123c6b1353
tm4s <tmas.szanto@gmail.com>
2016-03-02 14:52:51 +0100

    Orientation -> Colonel
    irany enum colonel-en belulre

--- src/Colonel.java
+++ src/Colonel.java
@@ -3,10 +3,34 @@
  */
 
 public class Colonel{
+    enum OrientationType {
+        NORTH, WEST, SOUTH, EAST
+    }
+
     private Coordinate position;
+    private Coordinate orientation;
+
+    public void setOrientation(OrientationType type) {
+        switch (type){
+            case NORTH:
+                orientation = new Coordinate(0,1);
+                break;
+            case WEST:
+                orientation = new Coordinate(-1,0);
+                break;
+            case SOUTH:
+                orientation = new Coordinate(0,-1);
+                break;
+            case EAST:
+                orientation = new Coordinate(1,0);
+                break;
+
+        }
+    }
 
     public Colonel(Coordinate position) {
         position = new Coordinate(position);
+        orientation = new Coordinate(0,1);      //NORTH
     }
 
     public void collideWith(Field field) {
