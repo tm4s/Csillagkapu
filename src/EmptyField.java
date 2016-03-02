547af45bd22dfaa32e6d6b10ca3f86a8a6440e04
tm4s <tmas.szanto@gmail.com>
2016-03-02 03:18:58 +0100

    EmptyField.java collideWithColonel

--- src/EmptyField.java
+++ src/EmptyField.java
@@ -3,4 +3,13 @@
  */
 
 public class EmptyField extends Field {
+
+    public EmptyField(Coordinate position) {
+        super(position);
+    }
+
+    @Override
+    public void accept(Colonel colonel) {
+        colonel.collideWith(this);
+    }
 }
