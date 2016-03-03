4f6b1c2b1d7288b41fe0b344e091d0c267f6c3db
tm4s <tmas.szanto@gmail.com>
2016-03-03 02:04:38 +0100

    wall default overrideok

--- src/Wall.java
+++ src/Wall.java
@@ -2,4 +2,18 @@
  * Sima fal
  */
 public class Wall extends Field {
+    @Override
+    public void collideWith(Colonel colonel) {
+
+    }
+
+    @Override
+    public void collideWith(Bullet bullet) {
+
+    }
+
+    @Override
+    public void collideWith(Box box) {
+
+    }
 }
