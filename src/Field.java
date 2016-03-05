66f07e5fb81fd46dc93a2a8f9eb500507ff9c06a
tm4s <tmas.szanto@gmail.com>
2016-03-05 03:15:25 +0100

    Field.java kommentelve

--- src/Field.java
+++ src/Field.java
@@ -7,14 +7,25 @@ public abstract class Field {
     private Coordinate position = new Coordinate(-1, -1);
     private Map map = null;
 
-
+    /**
+     * mezo szomszedai az iranyokat tarolo enumnak megfelelo az indexelese a tombnek
+     */
     private Field[] nextFields = new Field[4];
 
-
+    /**
+     * mezo adott iranyu szomszedjanak beallitasa
+     * @param direction ilyen iranyban helyezkedik el a szomszed
+     * @param field a szomszed mezo referenciaja
+     */
     public void setNextField(Orientation.Type direction, Field field) {
         nextFields[direction.ordinal()] = field;
     }
 
+    /**
+     * masik mezot helyez a sajat helyere
+     * eleg a masik mezo szomszedait atallitania ra
+     * @param field ez a mezo lep a helyebe
+     */
     public void setField(Field field) {
         field.setNextFields(nextFields);
 
@@ -24,14 +35,20 @@ public abstract class Field {
         field.setOnMap();
     }
 
+    /**
+     * megadott iranyu szomszed mezojet adja vissza
+     * @param direction ilyen iranyu szomszedjat
+     * @return adott iranyu szomszed mezo
+     */
     public Field getNextField(Orientation.Type direction) {
         return nextFields[direction.ordinal()];
     }
 
-    public Field[] getNextFields() {
-        return nextFields;
-    }
-
+    /**
+     * szomszed mezok ertesitese magarol es sajat szomszed mezoinek inicializalasa
+     * szomszed mezok megfelleo iranyu szomszed mezo referenciajat magara allitja
+     * @param nextFields szomszed mezok tombje
+     */
     public void setNextFields(Field[] nextFields) {
         for (int i = 0; i < nextFields.length; ++i) {
             this.nextFields[i] = nextFields[i];
@@ -39,10 +56,14 @@ public abstract class Field {
         }
     }
 
+    /**
+     * visitor pattern fuggvenyei
+     */
     public abstract void collideWith(Colonel colonel);
     public abstract void collideWith(Bullet bullet);
     public abstract void collideWith(Box box);
 
+    //teszteleshez kell
     public abstract Character print();
 
     //csak teszteleshez kell
