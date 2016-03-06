f400670a6f57fa5f9d3e047a7fc521a41520846b
tm4s <tmas.szanto@gmail.com>
2016-03-06 16:23:12 +0100

    ezredes keze mar tenyleg fent

---
+++ src/ColonelsHand.java
@@ -0,0 +1,15 @@
+/**
+ * Ezredes keze a doboz felvetelhez
+ */
+
+public class ColonelsHand {
+    private Colonel colonel;
+
+    public ColonelsHand(Colonel colonel) {
+        this.colonel = colonel;
+    }
+
+    public Colonel getColonel() {
+        return colonel;
+    }
+}
