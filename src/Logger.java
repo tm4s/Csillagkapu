61a643cc07a95c0adb1fb4766ce02bd8f7cccc88
BMark96 <markbereczki96@gmail.com>
2016-03-27 17:21:51 +0200

    Logger osztály

--- src/Logger.java
+++ src/Logger.java
@@ -2,10 +2,20 @@
  * Created by danielkrausz on 23/03/16.
  */
 public class Logger {
-
-    public static void Log() {
+	public static int numberOfTabs = -1;
+	
+    public static void Log(String parameter) {
+    	if(parameter.charAt(0) == '>') {
+    		++numberOfTabs;
+    	}
+    		
+    	for(int i = 0; i < numberOfTabs; ++i) {
+    		System.out.print("\t");
+    	}
+    	System.out.println(parameter);
     	
+    	if(parameter.charAt(0) == '<') {
+    		--numberOfTabs;
+    	}
     }
-
-
 }
