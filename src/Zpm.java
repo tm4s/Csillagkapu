2832c8c732e5c87722c7060fc27d29eca7919d3f
Benda Krisztián <krisztianbenda@gmail.com>
2016-03-27 18:25:44 +0200

    Zpm osztály logol már tényleg

--- src/Zpm.java
+++ src/Zpm.java
@@ -4,14 +4,17 @@
 public class Zpm extends Field {
 	@Override
 	public void collideWith(Colonel colonel) {
+		Logger.log(">Zpm.collideWith(Colonel colonel");
 		colonel.moveTo(this);
+		Logger.log("<Zpm.collideWith(Colonel colonel");
 	}
 
 	@Override
 	public void collideWith(Bullet bullet) {
+		Logger.log(">Zpm.collideWith(Bullet bullet");
 		bullet.moveForward();
+		Logger.log("<Zpm.collideWith(Bullet bullet");
 	}
-
 
     @Override
     public Character print() {
