c023e6490e45f4b169e6965d731be8d86e785453
tm4s <tmas.szanto@gmail.com>
2016-03-28 11:32:52 +0200

    menu es kilepes funkcio

--- src/Main.java
+++ src/Main.java
@@ -4,73 +4,15 @@ public class Main {
     
 	public static void main(String args[]) throws IOException {
 
-		String[] initText = {"Az objektumokat a karakteres kepernyon a kovetkezok jelolik:\n" +
-				"\tC: Colonel\n" +
-				"\t_: EmptyField\n" +
-				"\tB: Box\n" +
-				"\tD: Door\n" +
-				"\tS: Scale\n" +
-				"\tR: Ravine\n" +
-				"\t#: Wall\n" +
-				"\t+: SpecialWall\n" +
-				"\tZ: Zpm\n" +
-				"\t0/O: Teleporter\n\n"
-		};
+		boolean run = true;
 
-		String[] useCases = {"Adja meg a kert use-case szamat: \n" +
-				"1. Ures mezore lepes [C_]\n" +
-				"2. Ezredes utkozese\n" +
-				"\t2.1.fallal [C#]\n" +
-				"\t2.2 specialis fallal[C+]\n" +
-				"\t2.3 dobozzal[CB]\n" +
-				"\t2.4 ajtoval [CD]\n" +
-				"3. Merlegre fel- es lelepes [DCS]\n" +
-				"4. Szakadekba lepes [CR]\n" +
-				"5. Csillagkapuba lepes [0CO]\n" +
-				"6. ZPM begyujtese [CZ]\n" +
-				"7. Tolteny kilovese [C]\n" +
-				"8. Tolteny utkozese\n" +
-				"\t8.1. fallal [C#]\n" +
-				"\t8.2. ajtoval [CD]\n" +
-				"\t8.3. csillagkapuval [C0]\n" +
-				"9. Tolteny utkozese specialis fallal [C+]\n" +
-				"10. Tolteny utkozese ures mezovel, dobozzal, szakadekkal, merleggel, ZPM modullal\n" +
-				"11. Doboz lerakasa ures mezore [C_]\n" +
-				"12. Doboz lerakasa merlegre [CS]\n" +
-				"13. Doboz lerakasa szakadekba [CR]\n" +
-				"14. Sikertelen doboz lerakasa\n" +
-				"\t14.1 ajtora [CD]\n" +
-				"\t14.2 dobozra [CB]\n" +
-				"\t14.3 falra [C#]\n" +
-				"\t14.4 specialis falra [C+]\n" +
-				"\t14.5 ZPM modulra\n" +
-				"15. Doboz felvetele ures mezorol [CB]\n" +
-				"16. Doboz felvetele merlegrol [CBD]\n" +
-				"17. Sikertelen doboz felvetel\n" +
-				"\t17.1. ajtorol [CD]\n" +
-				"\t17.2. dobozrol [CB]\n" +
-				"\t17.3. falrol [C#]\n" +
-				"\t17.4. specialis falrol [C+]\n" +
-				"\t17.5. ures mezorol [C_]\n" +
-				"\t17.6. szakadekrol [CR]\n" +
-				"\t17.7. ZPM modulrol [CZ]\n"
-		};
-
-
-		for (int i = 0; i < initText.length; i++) {
-			System.out.println(initText[i]);
-		}
-
-		for (int i = 0; i < useCases.length; i++) {
-			System.out.println(useCases[i]);
-		}
-        
+		printMenu();
 
 		BufferedReader br = null;
 		br = new BufferedReader(new InputStreamReader(System.in));
 		String line = "";
 
-		while ((line = br.readLine()) != null) {
+		while (run && (line = br.readLine()) != null) {
 			switch (line) {
 				case "1.":
 					case1();
@@ -121,6 +63,13 @@ public class Main {
 					case16();
 					break;
 				case "17.":
+					break;
+				case "h":
+					printMenu();
+					break;
+				case "q":
+					run = false;
+					quit();
 					break;
 				default:
 					System.out.println("Adjon meg egy ervenyes esetet!");
@@ -496,4 +445,79 @@ public class Main {
         }
         
     }
+
+	public static void printMenu() {
+		String[] initText = {"Az objektumokat a karakteres kepernyon a kovetkezok jelolik:\n" +
+				"\tC: Colonel\n" +
+				"\t_: EmptyField\n" +
+				"\tB: Box\n" +
+				"\tD: Door\n" +
+				"\tS: Scale\n" +
+				"\tR: Ravine\n" +
+				"\t#: Wall\n" +
+				"\t+: SpecialWall\n" +
+				"\tZ: Zpm\n" +
+				"\t0/O: Teleporter\n\n"
+		};
+
+		String[] useCases = {"Adja meg a kert use-case szamat: \n" +
+				"1. Ures mezore lepes [C_]\n" +
+				"2. Ezredes utkozese\n" +
+				"\t2.1.fallal [C#]\n" +
+				"\t2.2 specialis fallal[C+]\n" +
+				"\t2.3 dobozzal[CB]\n" +
+				"\t2.4 ajtoval [CD]\n" +
+				"3. Merlegre fel- es lelepes [DCS]\n" +
+				"4. Szakadekba lepes [CR]\n" +
+				"5. Csillagkapuba lepes [0CO]\n" +
+				"6. ZPM begyujtese [CZ]\n" +
+				"7. Tolteny kilovese [C]\n" +
+				"8. Tolteny utkozese\n" +
+				"\t8.1. fallal [C#]\n" +
+				"\t8.2. ajtoval [CD]\n" +
+				"\t8.3. csillagkapuval [C0]\n" +
+				"9. Tolteny utkozese specialis fallal [C+]\n" +
+				"10. Tolteny utkozese ures mezovel, dobozzal, szakadekkal, merleggel, ZPM modullal\n" +
+				"11. Doboz lerakasa ures mezore [C_]\n" +
+				"12. Doboz lerakasa merlegre [CS]\n" +
+				"13. Doboz lerakasa szakadekba [CR]\n" +
+				"14. Sikertelen doboz lerakasa\n" +
+				"\t14.1 ajtora [CD]\n" +
+				"\t14.2 dobozra [CB]\n" +
+				"\t14.3 falra [C#]\n" +
+				"\t14.4 specialis falra [C+]\n" +
+				"\t14.5 ZPM modulra\n" +
+				"15. Doboz felvetele ures mezorol [CB]\n" +
+				"16. Doboz felvetele merlegrol [CBD]\n" +
+				"17. Sikertelen doboz felvetel\n" +
+				"\t17.1. ajtorol [CD]\n" +
+				"\t17.2. dobozrol [CB]\n" +
+				"\t17.3. falrol [C#]\n" +
+				"\t17.4. specialis falrol [C+]\n" +
+				"\t17.5. ures mezorol [C_]\n" +
+				"\t17.6. szakadekrol [CR]\n" +
+				"\t17.7. ZPM modulrol [CZ]\n" +
+				"\nh menu ujboli kiirasa\n" +
+				"q kilepes\n"
+		};
+
+
+		for (int i = 0; i < initText.length; i++) {
+			System.out.println(initText[i]);
+		}
+
+		for (int i = 0; i < useCases.length; i++) {
+			System.out.println(useCases[i]);
+		}
+	}
+
+	public static void quit() {
+		System.out.println();
+		System.out.println(Assets.LZ());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
