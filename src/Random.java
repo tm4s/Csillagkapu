688986e9bb50a035a1a993fbfbad84703c4dc2b4
danielkrausz <dnielkrausz@gmail.com>
2016-04-03 00:54:06 +0200

    Random

--- src/Random.java
+++ src/Random.java
@@ -2,5 +2,18 @@
  * Random generator
  */
 public class Random {
-    
+    boolean isTest = false;
+    int alma[] = {0, 1, 2, 3};
+
+    generateField() {}
+
+    void generate() {}
+
+    Orientation generateOrientation() {
+        Random rnd;
+        new Random();
+        int n = 3;
+        int i = rnd.nextInt() % n;
+
+    }
 }
