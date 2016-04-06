d699bc6fc7e324aac9037b4446303e6452cee59f
tm4s <tmas.szanto@gmail.com>
2016-04-06 02:12:30 +0200

    scale kezeli a sulyokat

--- src/Scale.java
+++ src/Scale.java
@@ -1,12 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Mérleg
  */
 
 public class Scale extends Field {
 	private Door door = new Door();
+	private int requiredWeight;
+	private int actualWeight;
+	private List<Box> boxes;
 
-	public Scale(Door door) {
+	public Scale(Door door, int requiredWeight) {
 		this.door = door;
+		this.requiredWeight = requiredWeight;
+		this.actualWeight = 0;
+		boxes = new ArrayList<>();
+	}
+
+	private void updateWeight(int weight) {
+		actualWeight += weight;
+		if (actualWeight >= requiredWeight)
+			door.open();
+		else
+			door.close();
+	}
+
+	public void addWeight(int plusWeight) {
+		updateWeight(plusWeight);
+	}
+
+	public void removeWeight(int minusWeight) {
+		updateWeight(-minusWeight);
+	}
+
+	public void addBox(Box box) {
+		boxes.add(box);
+		updateWeight(box.getWeight());
 	}
 
 	@Override
@@ -23,19 +53,15 @@ public class Scale extends Field {
 	public void collideWith(ColonelsHand hand) {
 		if (hand.hasBox())
 			hand.getColonel().boxPutDownToScale(this);
+		else if (!boxes.isEmpty()){
+			updateWeight(boxes.get(boxes.size()-1).getWeight());
+			hand.getColonel().boxPickUp(boxes.remove(boxes.size()-1));
+		}
 	}
 
 	@Override
 	public Character print() {
 		return 'S';
-	}
-
-	public void addWeight() {
-		door.open();
-	}
-
-	public void removeWeight() {
-		door.close();
 	}
 
 }
