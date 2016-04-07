83c7e8255b425437bea8aacbed29f98b642449b8
danielkrausz <dnielkrausz@gmail.com>
2016-04-07 10:52:22 +0200

    Teleport fix

--- src/Controller.java
+++ src/Controller.java
@@ -197,7 +197,8 @@ public class Controller {
     }
 
     private void printMap() {
-        System.out.print("---------------------------------------");
+        System.out.println("---------------------------------------");
+        System.out.println();
         Field nextField = firstField;
         Field nextRowFirstField = firstField.getNextField(Orientation.Type.SOUTH);
         while (nextField != null) {
