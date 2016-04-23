772840efc7284086318cda2a7a0f8dd01bb649e4
danielkrausz <dnielkrausz@gmail.com>
2016-04-23 17:00:30 +0200

    Forditas hozzaadasa

--- RunAllTests.cmd
+++ RunAllTests.cmd
@@ -1,5 +1,7 @@
 cd src
 
+javac *.java
+
 FOR /L %%i IN (1,1,9) DO java Main ../test/test0%%i.csv <../test/test0%%i.in >../test/test0%%i.out && java Validator ../test/test0%%i.e ../test/test0%%i.out
 
 FOR /L %%i IN (10,1,11) DO java Main ../test/test%%i.csv <../test/test%%i.in >../test/test%%i.out && java Validator ../test/test%%i.e ../test/test%%i.out
