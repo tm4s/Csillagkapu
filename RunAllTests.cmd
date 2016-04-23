afc57d945abede15d0c810e0fdb262651a759645
tm4s <tmas.szanto@gmail.com>
2016-04-23 16:21:25 +0200

    osszes teszt egyben valo futtatasa (src mappaban benne kell lennie az osszes class file-nak hszk-ban fog menni a javac nalam nem ment winen ezert hagytam meg igy)

---
+++ RunAllTests.cmd
@@ -0,0 +1,7 @@
+cd src
+
+FOR /L %%i IN (1,1,9) DO java Main ../test/test0%%i.csv <../test/test0%%i.in >../test/test0%%i.out && java Validator ../test/test0%%i.e ../test/test0%%i.out
+
+FOR /L %%i IN (10,1,11) DO java Main ../test/test%%i.csv <../test/test%%i.in >../test/test%%i.out && java Validator ../test/test%%i.e ../test/test%%i.out
+
+set /p temp="Hit enter to quit"
